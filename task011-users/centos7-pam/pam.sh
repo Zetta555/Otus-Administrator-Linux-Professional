c28c372a66985369bf14fa1bed759a6e5e1ad7d7
Aleksandr Kulishov <45900373+aleksandr-kulishov@users.noreply.github.com>
2021-01-19 13:32:51 +0300

    выполнение дз PAM

--- task011-users/centos7-pam/pam.sh
+++ task011-users/centos7-pam/pam.sh
@@ -7,8 +7,8 @@ echo "Otus2021" | passwd --stdin day
 echo "Otus2021" | passwd --stdin night
 echo "Otus2021" | passwd --stdin friday
 bash -c "sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd.service"
-echo '*;*;day;Al0800-2000
-*;*;night;!Al0800-2000
-*;*;friday;Fr' >> /etc/security/time.conf
-echo 'account    required     pam_nologin.so
-account    required     pam_time.so' >> /etc/pam.d/sshd
+
+groupadd admin
+usermod -a -G admin day
+usermod -a -G admin root
+usermod -a -G admin vagrant
