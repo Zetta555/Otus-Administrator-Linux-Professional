1a5792a64705ee250d5f9aeac90b0e0336377b8b
Aleksandr Kulishov <45900373+aleksandr-kulishov@users.noreply.github.com>
2021-01-20 15:06:33 +0300

    выполнение ДЗ Ansible-1

--- task011-users/centos7-pam/pam.sh
+++ task011-users/centos7-pam/pam.sh
@@ -7,7 +7,7 @@ echo "Otus2021" | passwd --stdin day
 echo "Otus2021" | passwd --stdin night
 echo "Otus2021" | passwd --stdin friday
 bash -c "sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd.service"
-
+setenforce 0
 groupadd admin
 usermod -a -G admin day
 usermod -a -G admin root
