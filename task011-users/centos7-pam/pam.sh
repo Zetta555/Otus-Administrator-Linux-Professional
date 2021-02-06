7cad09ad993309e28bff3c59f11f39328bd1f78f
Aleksandr Kulishov <45900373+aleksandr-kulishov@users.noreply.github.com>
2021-02-06 13:53:04 +0300

    ДЗ9,11 - в процессе

--- task011-users/centos7-pam/pam.sh
+++ task011-users/centos7-pam/pam.sh
@@ -9,6 +9,6 @@ echo "Otus2021" | passwd --stdin friday
 bash -c "sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd.service"
 setenforce 0
 groupadd admin
-usermod -a -G admin day
-usermod -a -G admin root
-usermod -a -G admin vagrant
+usermod -aG admin day
+usermod -aG admin root
+usermod -aG admin vagrant
