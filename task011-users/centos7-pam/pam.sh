35a053dd97674d766d883757eb910ce09c000a3d
Aleksandr Kulishov <45900373+aleksandr-kulishov@users.noreply.github.com>
2021-02-09 14:09:52 +0300

    change sh scritpt fo pam

--- task011-users/centos7-pam/pam.sh
+++ task011-users/centos7-pam/pam.sh
@@ -1,13 +1,38 @@
 #! /bin/bash
 
+yum install -y epel-release
+yum install -y pam_script
+
+# Установливаю Docker
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io mc
+systemctl enable docker
+systemctl start docker
+
+#
 useradd day
-useradd night
 useradd friday
+
 echo "Otus2021" | passwd --stdin day
-echo "Otus2021" | passwd --stdin night
 echo "Otus2021" | passwd --stdin friday
-bash -c "sed -i 's/^PasswordAuthentication.*$/PasswordAuthentication yes/' /etc/ssh/sshd_config && systemctl restart sshd.service"
-setenforce 0
+
 groupadd admin
-usermod -aG admin root
-usermod -aG admin vagrant
+usermod -aG admin day
+usermod -aG wheel day
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i -e '2 s/^/auth  required  pam_script.so\n/;' /etc/pam.d/sshd
+
+cat <<'EOT' > /etc/pam_script
+#!/bin/bash
+if [[ `grep $PAM_USER /etc/group | grep 'admin'` ]]
+then
+exit 0
+fi
+if [[ `date +%u` > 5 ]]
+then
+exit 1
+fi
+EOT
+chmod +x /etc/pam_script
+systemctl restart sshd
